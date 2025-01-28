cda8c74ce12eac1a68da32d9b0bc22febec0fb09
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-28 14:27:04 +0100

    ADD: loop

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const list = document.getElementById("list");
 
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    list.innerHTML += `<li>${data.response}</li>`
-})
-.catch(error => {
-    console.error(error)
-})
+for (i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            list.innerHTML += `<li>${data.response}</li>`
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
