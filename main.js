d409712422573898c0ca32749700a8b1cbb27c2e
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-28 14:33:44 +0100

    ADD: bonus button

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const list = document.getElementById("list");
+const btn = document.getElementById("btn");
 
-for (i = 0; i < 10; i++) {
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            list.innerHTML += `<li>${data.response}</li>`
-        })
-        .catch(error => {
-            console.error(error)
-        })
+function generateEmails() {
+    for (i = 0; i < 10; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                list.innerHTML += `<li>${data.response}</li>`
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
 }
+
+generateEmails();
+
+btn.addEventListener("click", function() {
+    list.innerHTML = "";
+    generateEmails();
+})
+
